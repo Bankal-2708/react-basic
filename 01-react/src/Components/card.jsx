7af6474bed9863fc2854b424fd432fe35eff7dfe
Banka-2708 <bankalmavi1@gmail.com>
2025-10-29 16:30:16 +0530

    Add More functions

---
+++ 01-react/src/Components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <div><div className="card">
+        <h1>JAI SHREE RAM</h1>
+        <p>jai shree Ram......</p></div>
+    </div>
+  )
+}
+
+export default Card
---
+++ 01-react/src/Components/navbar.jsx
@@ -0,0 +1,13 @@
+
+function Navbar() {
+  return (
+    <div>
+      <div className="navbar">
+        <h1>NavBar</h1>
+        <p>This is my nav bar</p>
+      </div>
+    </div>
+  ) 
+}
+
+export default Navbar
