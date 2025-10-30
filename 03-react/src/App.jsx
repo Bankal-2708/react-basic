0163bc50219b0ddbccfd1763c9dc23bedd4579b4
Banka-2708 <bankalmavi1@gmail.com>
2025-10-30 15:26:53 +0530

    Mini Project Complete

--- 03-react/src/App.jsx
+++ 03-react/src/App.jsx
@@ -4,8 +4,67 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <Card />
+    <div className='main'>
+      <Card 
+      logo="https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png"
+      Compnyname="Amazon"
+      releaseTime="5 days ago"
+      position="Senior UI/UX Designer"
+      workingTime="Part Time"
+      jobtype="Senior Level"
+      salary="$120/hr"
+      add="Mumbai, India"
+
+      />
+
+      <Card 
+      logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFFUTi1RsVDFSupmzDUJ5I3ZHBtwz043rbHQ&s"
+      Compnyname="Google"
+      releaseTime="30 days ago"
+      position="Graphic Designer"
+      workingTime="Part Time"
+      jobtype="Flexible Schedule"
+      salary="$115-220k"
+      add="Kochi, India"
+
+      />
+
+      <Card 
+      logo="https://cdn.freebiesupply.com/logos/large/2x/dribbble-icon-1-logo-png-transparent.png"
+      Compnyname="Dribbble "
+      releaseTime="18 days ago"
+      position="Senior Motion Designer"
+      workingTime="Contract"
+      jobtype="Remote"
+      salary="$85/hr"
+      add="Chennai, India"
+
+      />
+
+      <Card 
+      logo="https://images.icon-icons.com/2108/PNG/512/airbnb_icon_131000.png"
+      Compnyname="Airbnb "
+      releaseTime="5 days ago"
+      position="Junior UI/UX Designer"
+      workingTime="Contract"
+      jobtype="Remote"
+      salary="$100/hr"
+      add="Delhi, India"
+
+      />
+
+      <Card 
+      logo="https://www.shareicon.net/data/128x128/2016/11/24/856497_apple_512x512.png"
+      Compnyname="Apple "
+      releaseTime="5 days ago"
+      position="Graphic Designer"
+      workingTime="Full Time"
+      jobtype="Flexible Schedule"
+      salary="$85-120k"
+      add="Kerala, India"
+
+      />
+
     </div>
   );
 }
--- 03-react/src/Components/Card.jsx
+++ 03-react/src/Components/Card.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import {Bookmark} from "lucide-react";
 
-function Card() {
+function Card(props) {
   return (
     <div className='parent'>
       <div className='card'>
         <div className='top'>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/d/de/Amazon_icon.png" />
+          <img src={props.logo} />
           <button  >Save<Bookmark size={20} /></button>
         </div>
         <div className='centre'>
-            <h3>Amazon <span> 5 days ago</span></h3>
-            <h2>Senior UI/UX Designer</h2>
+            <h3>{props.Compnyname} <span> {props.realeseTime}</span></h3>
+            <h2>{props.position}</h2>
             <div className='tag'>
-              <h4>Part Time</h4>
-              <h4>Senior Level</h4>
+              <h4>{props.workingTime}</h4>
+              <h4>{props.jobtype}</h4>
             </div>
         </div>
         <div className='bottom'>
               <div>
-                  <h3>$120/hr</h3>
-                  <p>Mumbai, India</p>
+                  <h3>{props.salary}</h3>
+                  <p>{props.add}</p>
               </div>
               <div>
                   <button>Apply Now</button>
