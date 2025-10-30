5f1acf61cdaf4bd48e850714b830863e0f3fad68
Banka-2708 <bankalmavi1@gmail.com>
2025-10-30 11:38:13 +0530

    02-folder done

--- 02-react/src/App.jsx
+++ 02-react/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import Card from './compnents/Card'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='dis'>
+
+      {/* first user */}
+      <Card 
+      user='Bankal Mavi' 
+      data='Frontend Developer' 
+      img='https://plus.unsplash.com/premium_photo-1737699134443-b1add9135b54?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1074' 
+      profile='https://www.linkedin.com/in/bankal-mavi-646321273/' 
+      />
+
+      {/*  second user */}
+      <Card 
+      user='Ankush Kumar' 
+      data='Frontend Developer' 
+      img='https://plus.unsplash.com/premium_photo-1736967280298-1dc414b8a611?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332' 
+      profile='https://www.linkedin.com/in/ankush-kumar-76b0b9292/'
+      />
+      
+       {/* user third */}
+       <Card
+       user="Vanshika Poswal"
+       data="Backend Developer"
+       img="https://plus.unsplash.com/premium_photo-1737597231083-40b810942985?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1332"
+       profile="https://www.linkedin.com/in/vanshika-poswal-02138928b/" 
+       />
+      
+    </div>
   )
 }
 
